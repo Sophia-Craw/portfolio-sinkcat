1b0ad28ea0c2210337655e7aa5a0247de504ce52
Soph <145169616+Sophia-Craw@users.noreply.github.com>
2026-04-09 11:12:58 -0500

    self-hosting it is then

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,6 @@ import { solidStart } from "@solidjs/start/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  server: {
-    // @ts-ignore
-    start: {
-      server: {
-        preset: "cloudflare-pages",
-        rollupConfig: {
-          external: ["__STATIC_CONTENT_MANIFEST", "node:async_hooks"]
-        },
-      }
-    }
-  },
   plugins: [
     solidStart(),
     tailwindcss(),
